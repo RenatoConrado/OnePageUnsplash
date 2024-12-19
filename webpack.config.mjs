022fac4aaf18a145e27d8281d618ad71ede0bc8b
Renato <renatoconrado38@gmail.com>
2024-12-19 19:16:11 -0300

    css fix

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,6 +1,7 @@
 import path from "path";
 import { fileURLToPath } from "url";
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -21,12 +22,15 @@ export default {
       },
       {
         test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ], // Adiciona style-loader e css-loader
+        use: [ MiniCssExtractPlugin.loader, "css-loader" ],
       },
     ]
   },
   resolve: { extensions: [ ".js", ".jsx" ] },
-  plugins: [ new HtmlWebpackPlugin({ template: "./public/index.html" }) ],
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./public/index.html" }),
+    new MiniCssExtractPlugin({ filename: "[name].[contenthash].css" })
+  ],
   devServer: {
     static: path.join(__dirname, "public"),
     compress: true,
